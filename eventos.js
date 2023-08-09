f276cc3903ff39288ce2b0b6b8aed8944dc15800
SantiLarramendi <santiagolarramendi@gmail.com>
2023-08-09 19:29:54 -0300

    Trabajo realizado
    Las dos alertas ya están en funcionamiento

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const divElement = document.getElementById('saludarBoton');
+    
+    divElement.addEventListener('click', function () {
+        alert('Hola! Soy el div');
+    });
+});
